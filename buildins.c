afc0fe7069dc40c45e3703927fcbaf44468db592
Nnyeka <nnyeka212@gmail.com>
2022-11-17 09:05:13 -0800

    trying

--- buildins.c
+++ buildins.c
@@ -1,21 +1,25 @@
 #include "shell.h"
+/**
+ * lsh_num_builtins - returns builtins
+ */
 
-
-
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
+char *builtin_str[] = 
+{
+	 "cd",
+	 "help",
+	 "exit"
 };
 
-int (*builtin_func[]) (char **) = {
-  &get_cd,
-  &get_help,
-  &get_exit
+int (*builtin_func[]) (char **) = 
+{
+       	&get_cd,
+	&get_help,
+	&get_exit
 };
 
-int lsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
+int lsh_num_builtins() 
+{
+	return sizeof(builtin_str) / sizeof(char *);
 }
 
 /*
@@ -23,48 +27,52 @@ int lsh_num_builtins() {
 */
 
 /**
-   @brief Builtin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
+   get_cd - change directory.
+   @args: List of args.  args[0] is "cd".  args[1] is the directory.
+   @Return: Always returns 1, to continue executing.
  */
-int lsh_cd(char **args)
+int get_cd(char **args)
 {
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
+	if (args[1] == NULL) 
+	{
+		perror(stderr, "lsh: expected argument to \"cd\"\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("error");
+		}
+	}
+	return 1;
 }
 
 /**
-   @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
+   get_help - prints help
+   @args: List of args.  Not examined.
+   @Return: Always returns 1, to continue executing.
  */
-int lsh_help(char **args)
+int get_help(char **args)
 {
-  int i;
-  printf("Stephen Brennan's LSH\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
+       	int i;
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
-
-  printf("Use the man command for information on other programs.\n");
-  return 1;
+	printf("what is the problem?\n")
+		
+	
+	for (i = 0; i < lsh_num_builtins(); i++)
+	{
+		printf("  %s\n", builtin_str[i]);
+	}
+	printf("Use the man command for information on other programs.\n");
+	return 1;
 }
 
 /**
-   @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
+   get_exit - to exit
+   @args: arguments
+   @Return: Always returns 0, to terminate execution.
  */
-int lsh_exit(char **args)
+int get_exit(char **args)
 {
-  return 0;
+	return 0;
 }
--- get_exec.c
+++ get_exec.c
@@ -1,10 +1,79 @@
 #include "shell.h"
 
+
+char *builtin_str[] = {"cd", "help", "exit"};
+
+int (*builtin_func[]) (char **) = {&get_cd, &get_help, &get_exit};
+
+/**
+ * lsh_num_builtins - to return the builtins
+ *
+ * Return: the builtins
+ */
+
+int lsh_num_builtins(void)
+{
+	return (sizeof(builtin_str) / sizeof(char *));
+}
+
+/**
+ * get_cd -change dir
+ * @args: arguments
+ * Return: to return 1
+ */
+
+int get_cd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		perror("error: expected argument to \"cd\"\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("error");
+		}
+	}
+	return (1);
+}
+
+/**
+ * get_help - get help
+ * @args: arguments
+ * Return: to return 1 to continue
+ */
+
+int get_help(char **args)
+{
+	int i;
+
+	printf("the help section\n");
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
+	for (i = 0; i < lsh_num_builtins(); i++)
+	{
+		printf("  %s\n", builtin_str[i]);
+	}
+	printf("Use the man command to get information.\n");
+	return (1);
+}
+
+/**
+ * get_exit - to exit
+ * @args: arguments
+ * Return: to return 1
+ */
+
+int get_exit(char **args)
+{
+	return (0);
+}
+
 /**
  * get_exec - exceutes the shell builtins of launch
  * @args: list of args that is null terminated
- *
- * Return: 0 to terminate and 1 for continued
+ * Return: 0 to terminate else 1
  */
 
 int get_exec(char **args)
