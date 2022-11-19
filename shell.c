be5e0bbd0955a4d8b2b11594e9a84ce08e707515
Nnyeka <nnyeka212@gmail.com>
2022-11-18 20:45:06 -0800

    checks

--- shell.c
+++ shell.c
@@ -1,35 +1,89 @@
 #include "shell.h"
 
 /**
- * main - a simple UNIX command line interpreter
- * @ac: argument count
- * @av: argument vector
- *
- * Return: Always 0.
-*/
-int main(int ac, char **av)
+ * sig_handler - checks if Ctrl C is pressed
+ * @sig_num: int
+ */
+void sig_handler(int sig_num)
 {
-	/*run while loop in main entry point*/
-	while (true)
+	if (sig_num == SIGINT)
 	{
-		char *start = "$ ";
-		char *line;
-		char **args;
-
-		(void)ac;
-		(void)av;
-
-		printf("%s", start);
-		line = get_read_line();
-		args = get_tokenize_line(line);
-		if (args[0] != NULL)
-		{
-			get_exec(args);
-		}
-
-		free(line);
-		free(args);
-
-		return (0);
+		_puts("\n#cisfun$ ");
 	}
+}
+
+/**
+* _EOF - handles the End of File
+* @len: return value of getline function
+* @buff: buffer
+ */
+void _EOF(int len, char *buff)
+{
+	(void)buff;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("\n");
+			free(buff);
+		}
+		exit(0);
+	}
+}
+/**
+  * _isatty - verif if terminal
+  */
+
+void _isatty(void)
+{
+	if (isatty(STDIN_FILENO))
+		_puts("do$ ");
+}
+/**
+ * main - Shell
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, sig_handler);
+	while (len != EOF)
+	{
+		_isatty();
+		len = getline(&buff, &size, stdin);
+		_EOF(len, buff);
+		arv = splitstring(buff, " \n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
+		{
+			value = _getenv("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = checkbuild(arv);
+			if (f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
+			}
+		}
+	}
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return (0);
 }
